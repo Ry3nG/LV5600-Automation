8f5050b1eed26f5e090b43201a260d9ca4adc4f7
Gong Zerui <gong0060@e.ntu.edu.sg>
2023-06-09 17:33:49 +0800

    misc

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
-from scripts import capture_and_send
+from scripts import capture_and_send, change_preset, capture_and_send_performa
 import asyncio
+from Constants import Constants
 
 # Global variables
 IP_ADDRESS = "192.168.0.1"
@@ -7,7 +8,11 @@ USERNAME = "LV5600"
 PASSWORD = "LV5600"
 
 def main():
-    asyncio.run(capture_and_send.run(IP_ADDRESS, USERNAME, PASSWORD))
+    asyncio.run(capture_and_send_performa.run(Constants.IP_ADDRESS_TELNET, Constants.USERNAME_TELNET, Constants.PASSWORD_TELNET, Constants.IP_ADDRESS_FTP, Constants.USERNAME_FTP, Constants.PASSWORD_FTP))
+    
+    # change preset, first prompt user
+    #preset_number = input("Enter preset number: ")
+    #asyncio.run(change_preset.run(IP_ADDRESS, USERNAME, PASSWORD, preset_number))
 
 if __name__ == '__main__':
     main()
