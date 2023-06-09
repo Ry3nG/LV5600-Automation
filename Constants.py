52315e229d712892425d8573d34d874834b4bb2a
Gong Zerui <gong0060@e.ntu.edu.sg>
2023-06-09 17:32:33 +0800

    <structure> added Constants file, improve dependency

---
+++ Constants.py
@@ -0,0 +1,18 @@
+# the place for all the constants
+
+class Constants:
+        
+    # Credentials for FTP
+    IP_ADDRESS_FTP = "192.168.0.1"
+    USERNAME_FTP = "LV5600"
+    PASSWORD_FTP = "LV5600"
+
+    # Credentials for Telnet
+    IP_ADDRESS_TELNET = "192.168.0.1"
+    USERNAME_TELNET = "LV5600"
+    PASSWORD_TELNET = "LV5600"
+
+    # for Capture and storage
+    FTP_FILE_NAME_BMP = "cap_bmp.bmp"
+
+    LOCAL_FILE_PATH_BMP = "E:\\Leader LV5600\\LV5600 Automation\\output\\CAP_BMP.bmp"
--- scripts/capture_and_send.py
+++ scripts/capture_and_send.py
@@ -1,41 +1,44 @@
 from utils.telnet_client import TelnetClient
 from utils.credential import Credential
-from commands import capture_command
+from commands import capture_command, wfm_command
 from utils.ftp_client import FTPClient
 from time import sleep
 import logging
-
+from Constants import Constants
 
 # configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-async def run(ip_address, username, password):
+async def run(ip_address_telnet, username_telnet, password_telnet, ip_address_ftp, username_ftp, password_ftp):
     logging.info("Starting script: capture_and_send.py ...")
 
     # create a credential object
-    uut_credential = Credential(ip_address, username, password)
+    telnet_credential = Credential(ip_address_telnet, username_telnet, password_telnet)
+    ftp_credential = Credential(ip_address_ftp, username_ftp, password_ftp)
     logging.info("1. Credential object created ---")
 
     # create a telnet client object
-    client = TelnetClient(uut_credential)
+    client = TelnetClient(telnet_credential)
     logging.info("2. Telnet client object created ---")
+
+    # make sure to go to Waveform Monitor page
+    await client.send_command(wfm_command.goto_wfm_page())
+    sleep(0.2)
 
     # execute the command
     await client.send_command(capture_command.take_snapshot())
     logging.info("3. Take snapshot command executed ---")
     await client.send_command(capture_command.make("CAP_BMP"))
-    sleep(0.5)
+    sleep(0.4)
     logging.info("4. Make command executed ---")
 
     # create a ftp client object
-    ftp_client = FTPClient(uut_credential.ip_address, uut_credential.username, uut_credential.password)
+    ftp_client = FTPClient(ftp_credential.ip_address, ftp_credential.username, ftp_credential.password)
     logging.info("5. FTP client object created ---")
 
     # get the file
-    ftp_file_name = "cap_bmp.bmp"
-    local_file_path = "E:\\Leader LV5600\\LV5600 Automation with Telnet\\output\\CAP_BMP.bmp"
-    ftp_client.get_file(ftp_file_name, local_file_path)
-    logging.info("6. File downloaded, check the file at %s", local_file_path)
+    ftp_client.get_file(Constants.FTP_FILE_NAME_BMP, Constants.LOCAL_FILE_PATH_BMP)
+    logging.info("6. File downloaded, check the file at %s", Constants.LOCAL_FILE_PATH_BMP)
 
     # close the ftp connection
     ftp_client.close()
