311c6c8359eeac75918722af95df480b01930577
Gong Zerui <gong0060@e.ntu.edu.sg>
2023-06-15 17:34:03 +0800

    <add> <feat> added data analysis <feat> CNN <feat> Linear Regression <feat> autotestbench succesfully trained 3 different models, with accuracy up to 98.5%

--- Constants.py
+++ Constants.py
@@ -19,3 +19,20 @@ class Constants:
     # for Capture and storage
     FTP_FILE_NAME_BMP = "cap_bmp.bmp"
     LOCAL_FILE_PATH_BMP = "E:\\Leader LV5600\\LV5600 Automation\\output\\CAP_BMP.bmp"
+
+    # for waveform analysis
+    TEST_DATASET_PATH = "E:\\Data\\TestSet\\"
+    TEST_CATEGORIES = ['OverSaturated', 'UnderSaturated', 'JustSaturated']
+
+    MODEL_PATH = 'E:\\Model\\'
+    CNN_PREPROCESSING_BUFFER_PATH = 'E:\\Data\\PreprocessingBuffer\\'
+    FAILED_TESTCASES_PATH = "E:\\Data\\Failed Testset\\"
+    CROPPED_DATASET_PATH = 'E:\\Data\\LargeSet\\Cropped\\'
+
+    OVERSATURATED_DATASET_PATH = 'E:\\Data\\LargeSet\\OverSaturated'
+    UNDERSATURATED_DATASET_PATH = 'E:\\Data\\LargeSet\\UnderSaturated'
+    JUSTSATURATED_DATASET_PATH = 'E:\\Data\\LargeSet\\JustSaturated'
+
+    OVERSATURATED_CROPPED_DATASET_PATH = 'E:\\Data\\LargeSet\\Cropped\\OverSaturated'
+    UNDERSATURATED_CROPPED_DATASET_PATH = 'E:\\Data\\LargeSet\\Cropped\\UnderSaturated'
+    JUSTSATURATED_CROPPED_DATASET_PATH = 'E:\\Data\\LargeSet\\Cropped\\JustSaturated'
--- scripts/capture_and_send.py
+++ scripts/capture_and_send.py
@@ -28,3 +28,25 @@ async def capture_and_send_bmp(telnet_client, ftp_client):
         
     except Exception as e:
         logging.error(f"An error occurred: {e}")
+
+
+async def capture_and_send_bmp_to_name_path(telnet_client, ftp_client, file_path):
+    try:
+        # send the capture command
+        logging.info("Sending capture command...")
+        response = await telnet_client.send_command(capture_command.take_snapshot())
+        logging.info(f"Capture command response: {response}")
+        
+        # send the make command
+        logging.info("Sending make command...")
+        response = await telnet_client.send_command(capture_command.make("CAP_BMP"))
+        logging.info(f"Make command response: {response}")
+        
+        # get the file            logging.info("Getting file from FTP server...")
+        ftp_client.get_file(Constants.FTP_FILE_NAME_BMP, file_path)
+
+        logging.info("File downloaded successfully.")
+        
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        
