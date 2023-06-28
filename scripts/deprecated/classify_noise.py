ecdce92bb09cad6cbf9763b52e6027ac04af9fbd
Gong Zerui <gong0060@e.ntu.edu.sg>
2023-06-28 16:30:37 +0800

    deprecated

---
+++ scripts/deprecated/classify_noise.py
@@ -0,0 +1,17 @@
+import logging
+from .recall_preset import recall_preset
+from .capture_and_send_bmp import capture_and_send_bmp
+from time import sleep
+from utils.deprecated.noise_classifier import NoiseClassifier
+
+async def classify_noise(telnet_client, ftp_client, preset_number, file_path):
+    try:
+        await capture_and_send_bmp(telnet_client, ftp_client)
+        logging.info("Captured and sent bmp.")
+    except Exception as e:
+        logging.error(f"Failed to capture and send bmp: {e}")
+
+    NoiseClassifier_client = NoiseClassifier()
+    result = NoiseClassifier_client.classify_image(file_path)
+
+    return result
