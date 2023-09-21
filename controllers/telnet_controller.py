ca3646fd1ba671e8303442f90303f4d85ced3735
Gong Zerui <gong0060@e.ntu.edu.sg>
2023-09-21 13:01:02 +0800

    moved logging to gui class

--- controllers/telnet_controller.py
+++ controllers/telnet_controller.py
@@ -4,22 +4,23 @@ import telnetlib3
 from Constants import TelnetConstants
 import logging
 
+
 class TelnetController:
     """
-            A class that handles Telnet connections and commands.
+    A class that handles Telnet connections and commands.
 
-            Attributes:
-                host (str): The IP address or hostname of the Telnet server.
-                port (int): The port number of the Telnet server.
-                username (str): The username to use for authentication.
-                password (str): The password to use for authentication.
-                reader (Optional[telnetlib3.StreamReader]): The Telnet reader object.
-                writer (Optional[telnetlib3.StreamWriter]): The Telnet writer object.
-                end_string (bytes): The string that marks the end of a Telnet response.
-                connection_timeout (float): The maximum time to wait for a Telnet connection.
+    Attributes:
+        host (str): The IP address or hostname of the Telnet server.
+        port (int): The port number of the Telnet server.
+        username (str): The username to use for authentication.
+        password (str): The password to use for authentication.
+        reader (Optional[telnetlib3.StreamReader]): The Telnet reader object.
+        writer (Optional[telnetlib3.StreamWriter]): The Telnet writer object.
+        end_string (bytes): The string that marks the end of a Telnet response.
+        connection_timeout (float): The maximum time to wait for a Telnet connection.
     """
 
-    def __init__(self,host,port,username,password):
+    def __init__(self, host, port, username, password):
         self.host = host
         self.port = port
         self.username = username
@@ -28,13 +29,12 @@ class TelnetController:
         self.writer = None
         self.end_string = TelnetConstants.TELNET_END_STRING
         self.connection_timeout = TelnetConstants.TELNET_CONNECTION_TIMEOUT
-        
-            
+
     async def login(self):
         """
-            Logs in to the Telnet server using the provided username and password.
-            If the writer or reader is None, raises a ConnectionError.
-            """
+        Logs in to the Telnet server using the provided username and password.
+        If the writer or reader is None, raises a ConnectionError.
+        """
         try:
             if self.writer is not None and self.reader is not None:
                 await self.reader.readuntil(b"login: ")
@@ -45,7 +45,6 @@ class TelnetController:
             else:
                 raise ConnectionError("Error logging in: writer or reader is None")
         except Exception as e:
-            logging.error(f"Error while logging in: {str(e)}")
             raise Exception(f"Error while logging in: {str(e)}")
 
     async def connect(self):
@@ -56,30 +55,29 @@ class TelnetController:
         try:
             self.reader, self.writer = await asyncio.wait_for(
                 telnetlib3.open_connection(self.host, self.port),
-                timeout=self.connection_timeout
+                timeout=self.connection_timeout,
             )
         except asyncio.TimeoutError:
-            logging.error("Telnet connection timed out")
             raise ConnectionError("Telnet connection timed out")
         except Exception as e:
             raise Exception(f"Error while connecting to telnet: {str(e)}")
 
     async def send_command(self, command):
         """
-                Sends a command to the Telnet server and returns the response.
-                If the writer or reader is None, raises a ConnectionError.
-                If the command times out, raises a TimeoutError.
+        Sends a command to the Telnet server and returns the response.
+        If the writer or reader is None, raises a ConnectionError.
+        If the command times out, raises a TimeoutError.
         """
         if self.writer is None or self.reader is None:
-            logging.error("Error sending command: writer or reader is None")
             raise ConnectionError("Error sending command: writer or reader is None")
 
         response = None
         try:
             self.writer.write(command + "\r\n")
-            response = await asyncio.wait_for(self.reader.readuntil(self.end_string), timeout=self.connection_timeout)
+            response = await asyncio.wait_for(
+                self.reader.readuntil(self.end_string), timeout=self.connection_timeout
+            )
         except Exception as e:
-            logging.error(f"Error while sending command: {str(e)}")
             raise Exception(f"Error while sending command: {str(e)}")
 
         return response
@@ -94,10 +92,8 @@ class TelnetController:
                 self.writer.close()
             else:
                 logging.warning("Writer is None")
-                
+
         except ConnectionError as e:
-            logging.error(f"Error while closing connection: {str(e)}")
             raise ConnectionError(f"Error while closing connection: {str(e)}")
         except Exception as e:
-            logging.error(f"Error while closing connection: {str(e)}")
             raise Exception(f"Error while closing connection: {str(e)}")
