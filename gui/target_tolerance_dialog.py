1d08213fb499dfec389db2841c76f616df31eb00
Gong Zerui <gong0060@e.ntu.edu.sg>
2023-09-21 15:58:06 +0800

    fixed typo

--- gui/target_tolerance_dialog.py
+++ gui/target_tolerance_dialog.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QDialog, QLineEdit, QPushButton, QVBoxLayout, QLabel
 
 class TargetToleranceDialog(QDialog):
     # target tolerance involves 3 params
-        # 1. target tolerance : 0.00% to 1%
+        # 1. target tolerance : 0% to 100%
         # 2. flatness_check_pixel: 0 pixel to 200 pixel
         # 3. flatness_check_sv_threshold: 0.0 to 100.0
     def __init__(self, app_config):
@@ -14,10 +14,10 @@ class TargetToleranceDialog(QDialog):
 
         # create widgets, using spinbox 
         self.target_tolerance_spinbox = QtWidgets.QDoubleSpinBox()
-        self.target_tolerance_spinbox.setRange(0.00, 1.00)
-        self.target_tolerance_spinbox.setSingleStep(0.01)
-        self.target_tolerance_spinbox.setValue(self.app_config.get_target_tolerance())
-        self.target_tolerance_spinbox.setDecimals(2)
+        self.target_tolerance_spinbox.setRange(0,100)
+        self.target_tolerance_spinbox.setSingleStep(0.1)
+        self.target_tolerance_spinbox.setValue(self.app_config.get_target_tolerance()*100)
+        self.target_tolerance_spinbox.setDecimals(1)
         self.target_tolerance_spinbox.setSuffix("%")
         self.flatness_check_pixel_spinbox = QtWidgets.QSpinBox()
         self.flatness_check_pixel_spinbox.setRange(0, 200)
@@ -49,9 +49,9 @@ class TargetToleranceDialog(QDialog):
 
     def save_settings(self):
         # update app_config
-        self.app_config.set_target_tolerance(self.target_tolerance_spinbox.value())
-        self.app_config.set_flatness_check_pixel(self.flatness_check_pixel_spinbox.value())
-        self.app_config.set_flatness_check_sv_threshold(self.flatness_check_sv_threshold_spinbox.value())
+        self.app_config.set_target_tolerance(float(self.target_tolerance_spinbox.value()/100))
+        self.app_config.set_flatness_check_pixel(float(self.flatness_check_pixel_spinbox.value()))
+        self.app_config.set_flatness_check_sv_threshold(float(self.flatness_check_sv_threshold_spinbox.value()))
 
         # update config.ini
         self.app_config.save_config_to_file()
